d5a46f65b01319b24f9cf91cd2eed93efc6b0da3
helloitsdaksh <helloitsdaksh007@gmail.com>
2021-05-22 18:36:56 +0530

    defined the TIC TAC TOE logic

--- XO.py
+++ XO.py
@@ -14,3 +14,27 @@ def choice():
     elif player_one.upper() == "O":
         player_two = "X"
     return player_one,player_two
+
+def rowcrossed(grid):
+
+    for i in range(3):
+        if grid[i][0] == grid[i][1] and grid[i][1] == grid[i][2] and grid[i][0] != ' ':
+            return (True)
+    return False
+
+def columncrossed(grid):
+
+    for i in range(3):
+        if grid[0][i] == grid[1][i] and grid[1][i] == grid[2][i] and grid[0][i] != ' ':
+            return (True)
+    return False
+
+def diagonalcrossed(grid):
+
+    if grid[0][0] == grid[1][1] and grid[1][1] == grid[2][2] and grid[0][0] != ' ':
+        return (True)
+
+    if grid[0][2] == grid[1][1] and grid[1][1] == grid[2][0] and grid[0][2] != ' ':      
+        return (True)
+  
+    return(False)
