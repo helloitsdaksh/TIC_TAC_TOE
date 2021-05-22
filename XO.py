0370e5ff2064bf450494ae17676f8963a5d1b714
helloitsdaksh <helloitsdaksh007@gmail.com>
2021-05-22 18:38:55 +0530

    made changes in __name__

--- XO.py
+++ XO.py
@@ -78,3 +78,12 @@ def entry(one,two,count):
                         print(np.matrix(grid))
                         entry(one,two,count)
     
+
+
+if __name__ == "__main__":
+    count = 0
+    print(np.matrix(grid))
+    x,y = choice()
+    print({'Player One' : x,'Player Two' : y})
+    entry(x,y,count)
+    print(np.matrix(grid))
