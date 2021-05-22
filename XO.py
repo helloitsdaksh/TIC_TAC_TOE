5dc4d38cb6229971bc7abea2948192952d44c028
helloitsdaksh <helloitsdaksh007@gmail.com>
2021-05-22 18:41:00 +0530

    comments added

--- XO.py
+++ XO.py
@@ -7,6 +7,10 @@ grid = [["1","2","3"],
 player_one = ""
 player_two = ""
 
+
+"""
+PLAYER HAS TO DECIDE WHAT HE WANTS??
+"""
 def choice():
     player_one = input("Enter Your Choice Player one, 'X' or 'O':")
     if player_one.upper() == "X":
@@ -14,7 +18,9 @@ def choice():
     elif player_one.upper() == "O":
         player_two = "X"
     return player_one,player_two
-
+"""
+LOGIC OF TIC TAC TOE
+"""
 def rowcrossed(grid):
 
     for i in range(3):
@@ -39,10 +45,17 @@ def diagonalcrossed(grid):
   
     return(False)
 
+"""
+GAME OVER DECIDER
+"""
+
 def gameOver(grid):
 
     return (rowcrossed(grid) or columncrossed(grid) or diagonalcrossed(grid))
 
+"""
+PLAYER CHOICE TO BE APPENDED IN THE GRID(MATRIX)
+"""
 
 def entry(one,two,count):
     # position = {'1':'grid[0][0]','2':grid[0][1], '3':grid[0][2], '4':grid[1][0], '5':grid[1][1],'6':grid[1][2],'7':grid[2][0],'8':grid[2][1],'9':grid[2][2]}
@@ -79,7 +92,9 @@ def entry(one,two,count):
                         entry(one,two,count)
     
 
-
+"""
+CALL THE FUNCTION
+"""
 if __name__ == "__main__":
     count = 0
     print(np.matrix(grid))
